559514af9410ce75f78e1612199187a22144bc77
bdgbao <elquellora04@gmail.com>
2026-05-18 01:30:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,10 @@ def feature_engineering(raw_df):
 if st.button("Dự đoán UPDRS", type="primary", use_container_width=True):
     
     input_data = pd.DataFrame([{
-        'age': age, 'sex': sex, 'test_time': test_time,
+        'age': age, 'test_time': test_time,
         'Jitter(%)': jitter_percent, 'Jitter(Abs)': jitter_abs, 'Jitter:RAP': jitter_rap, 'Jitter:PPQ5': jitter_ppq5, 'Jitter:DDP': jitter_ddp,
         'Shimmer': shimmer, 'Shimmer(dB)': shimmer_db, 'Shimmer:APQ3': shimmer_apq3, 'Shimmer:APQ5': shimmer_apq5, 'Shimmer:APQ11': shimmer_apq11, 'Shimmer:DDA': shimmer_dda,
-        'NHR': nhr, 'HNR': hnr, 'RPDE': RPDE, 'DFA': DFA, 'PPE': PPE
+        'NHR': nhr, 'sex': sex, 'HNR': hnr, 'RPDE': RPDE, 'DFA': DFA, 'PPE': PPE
     }])
     
     processed_df = feature_engineering(input_data)
